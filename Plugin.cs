d14d061a856138d7c0c8396c7ea82efe6a43eec3
bobjrsenior <git@bobjrsenior.pw>
2022-06-22 08:12:15 -0700

    Create simple wrapper plugin for System.Text.Json

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using BepInEx.IL2CPP;
+
+namespace JsonLibs
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        public override void Load()
+        {
+            // Nothing needed
+        }
+    }
+}
